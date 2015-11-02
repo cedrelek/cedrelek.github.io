fb1433b971e4847966d0cc84c3e3bd57eb66bd10
Cedric Lechevrel <cedrelek@gmail.com>
2015-11-02 07:50:31 +0100

    update site : addall

---
+++ index.js
@@ -0,0 +1,51 @@
+angular.module("ZomeBuilder", ["GeneralShapeModule",
+                               "ViewModule",
+                               "LevelDefinitionModule",
+                               'MathModule',
+                               "ZomeDefinitionModule",
+                               "WallDefinitionModule",
+                               "ZomeValidationModule",
+                               "ZomeEdgeViewModule",
+                               "LogDimensionModule"])
+  .directive("generalShapeComp", function() {
+    return {
+      templateUrl: 'components/generalShape.html',
+      restrict: 'E',
+      replace: true
+    }
+  })
+  .directive("levelDefinitionComp", function() {
+    return {
+      templateUrl : 'components/levelDefinition.html',
+      restrict: 'E',
+      replace: true
+    }
+  })
+  .directive("wallDefinitionComp", function() {
+    return {
+      templateUrl : 'components/wallDefinition.html',
+      restrict: 'E',
+      replace: true
+    }
+  })
+  .directive("zomeValidationComp", function() {
+    return {
+      templateUrl : 'components/zomeValidation.html',
+      restrict: 'E',
+      replace: true
+    }
+  })
+  .directive("zomeEdgeComp", function() {
+    return {
+      templateUrl : 'components/zomeEdgeView.html',
+      restrict: 'E',
+      replace: true
+    }
+  })
+  .directive("logDimensionComp", function() {
+    return {
+      templateUrl : 'components/logDimension.html',
+      restrict: 'E',
+      replace : true
+    }
+  });
